749bcef9c3a2e27847f33c6ae009b211fceea8a3
alsaiduq-labs <riiidge.racer@gmail.com>
2023-11-11 18:26:47 -0700

    whoops

--- pokeduel/__init__.py
+++ pokeduel/__init__.py
@@ -3,11 +3,12 @@ from pathlib import Path
 
 from redbot.core.bot import Red
 
-from .pokeduel import pokeduel
+from .pokeduel import PokeDuel
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
 async def setup(bot: Red) -> None:
-    await bot.add_cog(pokeduel(bot))
+    cog_instance = PokeDuel(bot)
+    await bot.add_cog(cog_instance)
